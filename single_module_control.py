3ffb8af10389ba77cfd3f45d4f5586d9dfc86984
ruixinl6 <44179096+ruixinl6@users.noreply.github.com>
2020-02-25 22:56:39 -0500

    Add files via upload

---
+++ single_module_control.py
@@ -0,0 +1,26 @@
+print('This code sends position commands through serial port to O=6 Module.')
+
+import serial
+import time
+
+
+DEBUG_NO_SERIAL = 0
+ser = serial.Serial(port = 'ttyUSB0', baudrate = '115200')
+
+
+if DEBUG_NO_SERIAL == 0:
+    ser.write('FFO')  # scan online modules
+    msg = ser.readline()
+    print('Scanned online modules: ' +msg)
+
+start = time.time()
+while True:
+    time.sleep(0.1)
+    ser.write('1AP65535R\n') # From Lu's Matlab version, it says that the encoder starts from 65535.
+    print('Position command sent.')
+    end = time.time() 
+    if end-start > 3:
+        ser.close()
+        exit()
+
+
