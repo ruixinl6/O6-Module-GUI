373db47e386bb91f54aec87f087ff94beb1592cd
ruixinli <ruixinli@github.com>
2020-02-26 16:44:48 -0500

    position commands work

--- single_module_control.py
+++ single_module_control.py
@@ -5,19 +5,19 @@ import time
 
 
 DEBUG_NO_SERIAL = 0
-ser = serial.Serial(port = 'ttyUSB0', baudrate = '115200')
+ser = serial.Serial(port = '/../../dev/ttyACM0', baudrate = '115200')
 
 
 if DEBUG_NO_SERIAL == 0:
-    ser.write('FFO')  # scan online modules
+    ser.write('FFO\n')  # scan online modules
     msg = ser.readline()
     print('Scanned online modules: ' +msg)
 
 start = time.time()
 while True:
     time.sleep(0.1)
-    ser.write('1AP65535R\n') # From Lu's Matlab version, it says that the encoder starts from 65535.
-    print('Position command sent.')
+    ser.write('FFP29000R\n') # From Lu's Matlab version, it says that the encoder starts from 65535.
+    # print('Position command sent.')
     end = time.time() 
     if end-start > 3:
         ser.close()
